9a28bed7a2b1b2db37234c502e0335e96c473803
uk2459644 <uk2459644@gmail.com>
2021-09-29 16:26:48 +0530

    button of top service removed

--- components/instituecards/herocomponent.js
+++ components/instituecards/herocomponent.js
@@ -34,10 +34,10 @@ style={{
       <p className=" mt-3 text-white sm:mt-5 sm:max-w-xl md:mt-5 text-lg font-light">
       {item.description}</p>
       <div className="mt-5 sm:mt-8 sm:flex justify-start">
-        <div className="rounded-md shadow">
+        {/* <div className="rounded-md shadow">
           <a href="#" className="w-full  flex items-center justify-center px-8 py-3 border border-transparent text-base leading-6 font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo transition duration-150 ease-in-out md:py-4 md:text-lg md:px-10"> Get started </a>
         </div>
-       
+        */}
       </div>
     </div>
   </main>
